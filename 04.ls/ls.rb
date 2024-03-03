c1bd1ff8f9eaa0032fe4df7c273a398d3f49996a
takayuki <takayuki12010218@gmail.com>
2024-03-04 08:50:00 +0900

    add r option to ls

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,35 +5,49 @@ require 'optparse'
 NUM_COLUMNS = 3 # 列幅の最大数
 
 def main
-  taken_items = take_items
-  sliced_items = slice_items(taken_items)
+  options = parse_options
+  taken_items = take_items(options)
+  sliced_items = slice_items(taken_items, options)
   transposed_items = transpose_items(sliced_items)
   display_items(transposed_items)
 end
 
-def take_items
-  take_hidden_files = false
+def parse_options
+  options = { take_hidden_files: false, reverse: false }
 
   OptionParser.new do |opt|
     opt.on('-a') do
-      take_hidden_files = true
+      options[:take_hidden_files] = true
+    end
+
+    opt.on('-r') do
+      options[:reverse] = true
     end
   end.parse!
 
-  if take_hidden_files
+  options
+end
+
+def take_items(options)
+  if options[:take_hidden_files]
     Dir.foreach('.').to_a
   else
     Dir.glob('*')
   end.sort
 end
 
-def slice_items(taken_items)
+def slice_items(taken_items, options)
   slice_number = if (taken_items.size % NUM_COLUMNS).zero?
                    taken_items.size / NUM_COLUMNS.ceil # NUM_COLUMNSの倍数の時だけ、NUM_COLUMNSで割り込む
                  else
                    taken_items.size / NUM_COLUMNS.ceil + 1 # 最大NUM_COLUMNS列に収める
                  end
-  taken_items.each_slice(slice_number).to_a
+
+  if options[:reverse]
+    taken_items.reverse.each_slice(slice_number).to_a
+  else
+    taken_items.each_slice(slice_number).to_a
+  end
 end
 
 def transpose_items(sliced_items)
