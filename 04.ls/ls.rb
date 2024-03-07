9c7ee3d40573aa6b76eec89103e10d346f558bca
takayuki <takayuki12010218@gmail.com>
2024-03-07 21:36:14 +0900

    reverseオプションの場所をtake_itemメソッドに移動

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,7 +7,7 @@ NUM_COLUMNS = 3 # 列幅の最大数
 def main
   options = parse_options
   taken_items = take_items(options)
-  sliced_items = slice_items(taken_items, options)
+  sliced_items = slice_items(taken_items)
   transposed_items = transpose_items(sliced_items)
   display_items(transposed_items)
 end
@@ -29,25 +29,26 @@ def parse_options
 end
 
 def take_items(options)
-  if options[:take_hidden_files]
-    Dir.foreach('.').to_a
+  entries = if options[:take_hidden_files]
+              Dir.foreach('.').to_a
+            else
+              Dir.glob('*')
+            end
+  if options[:reverse]
+    entries.sort.reverse
   else
-    Dir.glob('*')
-  end.sort
+    entries.sort
+  end
 end
 
-def slice_items(taken_items, options)
+def slice_items(taken_items)
   slice_number = if (taken_items.size % NUM_COLUMNS).zero?
                    taken_items.size / NUM_COLUMNS.ceil # NUM_COLUMNSの倍数の時だけ、NUM_COLUMNSで割り込む
                  else
                    taken_items.size / NUM_COLUMNS.ceil + 1 # 最大NUM_COLUMNS列に収める
                  end
 
-  if options[:reverse]
-    taken_items.reverse.each_slice(slice_number).to_a
-  else
-    taken_items.each_slice(slice_number).to_a
-  end
+  taken_items.each_slice(slice_number).to_a
 end
 
 def transpose_items(sliced_items)
